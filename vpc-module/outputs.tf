70b16be4c6a753a283c01435e11e9602277b4f76
deepak9494 <busadeepak61@gmail.com>
2025-06-04 13:03:55 +0530

    Update outputs.tf

--- vpc-module/outputs.tf
+++ vpc-module/outputs.tf
@@ -1,4 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
 output "app_subnet_ids" {
-  description = "List of app subnet IDs"
+  description = "List of app/private subnet IDs"
   value       = aws_subnet.app[*].id
+}
+
+output "db_subnet_ids" {
+  description = "List of database subnet IDs"
+  value       = aws_subnet.db[*].id
+}
+
+output "public_route_table_ids" {
+  description = "Public route table IDs"
+  value       = aws_route_table.public[*].id
+}
+
+output "private_route_table_ids" {
+  description = "Private route table IDs"
+  value       = aws_route_table.private[*].id
 }
